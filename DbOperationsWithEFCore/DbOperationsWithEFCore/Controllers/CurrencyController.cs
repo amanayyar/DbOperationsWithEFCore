0c8f83e670d9d33caefa9353b3013ca4b799c0c6
Aman Nayyar <aman.nayyar21@gmail.com>
2023-08-28 16:07:35 +0530

    Get All Records from Table using Entity Framework Core & LINQ (Sync & Async)

---
+++ DbOperationsWithEFCore/DbOperationsWithEFCore/Controllers/CurrencyController.cs
@@ -0,0 +1,38 @@
+﻿using DbOperationsWithEFCore.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DbOperationsWithEFCore.Controllers
+{
+	[Route("api/currencies")]
+	[ApiController]
+	public class CurrencyController : ControllerBase
+	{
+		private readonly AppDbContext _appDbContext;
+
+		public CurrencyController(AppDbContext appDbContext)
+		{
+			_appDbContext = appDbContext;
+		}
+
+		[HttpGet("")]
+		public IActionResult GetAllCurrencies()
+		{
+			//var result = _appDbContext.Currencies.ToList();
+			var result = (from currencies in _appDbContext.Currencies
+						 select currencies).ToList();
+			return Ok(result);
+		}
+		[HttpGet("GetAllCurrenciesAsync")]
+		public async Task<IActionResult> GetAllCurrenciesAsync()
+		{
+			//var result = await _appDbContext.Currencies.ToListAsync();
+			var result = await (from currencies in _appDbContext.Currencies
+						  select currencies).ToListAsync();
+			return Ok(result);
+		}
+
+	}
+}
