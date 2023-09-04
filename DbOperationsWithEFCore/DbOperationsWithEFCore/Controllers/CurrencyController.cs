03d5e9bbef0a0e2e6d614b97ab7c8d8194bc3cc5
Aman Nayyar <aman.nayyar21@gmail.com>
2023-09-04 15:45:56 +0530

    Get Records based on Ids [1,2,3....n] from entity framework core

--- DbOperationsWithEFCore/DbOperationsWithEFCore/Controllers/CurrencyController.cs
+++ DbOperationsWithEFCore/DbOperationsWithEFCore/Controllers/CurrencyController.cs
@@ -57,5 +57,15 @@ namespace DbOperationsWithEFCore.Controllers
 			return Ok(result);
 		}
 
+		[HttpPost("all")]
+		public async Task<IActionResult> GetCurrenciesByIdsAsync([FromBody] List<int> ids)
+		{
+			//var ids = new List<int> { 1, 6, 2, 3 };
+			var result = await _appDbContext.Currencies
+				.Where(x => ids.Contains(x.Id))
+				.ToListAsync();
+			return Ok(result);
+		}
+
 	}
 }
